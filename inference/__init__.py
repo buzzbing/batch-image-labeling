ab7bcdfe7773ee0ec923b7738c778a3d04d0c583
buzzbing <dnamrata.2016@gmail.com>
2025-08-15 10:29:52 +0545

    Chore: bugfix redundant code removed

---
+++ inference/__init__.py
@@ -0,0 +1,3 @@
+from .predictor import FishDetector
+
+__all__ = ['FishDetector']
--- inference/predictor.py
+++ inference/predictor.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torchvision
 import json
@@ -11,8 +12,9 @@ from io import BytesIO
 
 IOU_THRESHOLD = 0.5
 SCORE_THRESHOLD = 0.5
-TEMPLATE_PATH = 'inference/coco_template.json'
+TEMPLATE_PATH = "inference/coco_template.json"
 TRANSFORM_PATH = "inference/transforms.json"
+
 
 class FishDetector:
     def __init__(self, class_mapping_path: str, modelpath: str):
@@ -22,7 +24,7 @@ class FishDetector:
         self.model = None
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    def _load_model(self):
+    def load_model(self):
         with open(self.class_mapping_path) as data:
             mappings = json.load(data)
         self.class_mapping = {
@@ -30,28 +32,34 @@ class FishDetector:
         }
         self.model = torch.jit.load(self.modelpath).to(self.device)
 
-    def get_image_from_url(self, image_url:str):
-        response = requests.get(image_url)
-        image = Image.open(BytesIO(response.content)).convert('RGB')
+    def get_image(self, image_path: str):
+        if image_path.startswith(("http://", "https://")):
+            response = requests.get(image_path)
+            response.raise_for_status()
+            image = Image.open(BytesIO(response.content)).convert("RGB")
+        else:
+            if not os.path.exists(image_path):
+                raise FileNotFoundError(f"File not found: {image_path}")
+            image = Image.open(image_path).convert("RGB")
+
         return image
-    
+
     def transform_image(
         self,
-        image_url: str,
+        image_path: str,
         configpath: str = TRANSFORM_PATH,
     ) -> Image:
-        image = self.get_image_from_url(image_url)
+        image = self.get_image(image_path)
         transforms = A.load(configpath)
         image_np = np.array(image)
         image = transforms(image=image_np)["image"]
         return image
 
-    def predict(self, image_url:str) -> list:
-        self._load_model()
-        image = self.transform_image(image_url)
+    def predict(self, image_path: str) -> list:
+        self.load_model()
+        image = self.transform_image(image_path)
         x = torch.from_numpy(image).to(self.device)
         with torch.no_grad():
-            # Convert to channels first, convert to float datatype
             x = x.permute(2, 0, 1).float()
             y = self.model(x)
             pred_boxes = y["pred_boxes"]
@@ -86,10 +94,9 @@ class FishDetector:
                 )
 
             return results
-            
 
-    def draw_boxes_on_image(self, image_url: str, coco_predictions: dict):
-        image = self.transform_image(image_url)
+    def draw_boxes_on_image(self, image_path: str, coco_predictions: dict):
+        image = self.transform_image(image_path)
         image_draw = np.array(image).copy()
         for pred in coco_predictions:
             x, y, w, h = map(int, pred["bbox"])
@@ -112,78 +119,79 @@ class FishDetector:
         plt.imshow(image_draw)
         plt.show()
 
-    
-
-    def get_coco_annotation(self, image_url, predicted_boxes):
+    def get_coco_annotation(self, image_path, predicted_boxes):
         """
         Generates a COCO format dictionary for an image and its annotations.
         """
-        
-        image = self.get_image_from_url(image_url)
-        width, height = image.size
 
+        image = self.get_image(image_path)
+        width, height = image.size
 
         with open(TEMPLATE_PATH) as file:
             coco_output = json.load(file)
-        
-        for k,v in self.class_mapping.items():
 
-            coco_output["categories"].append({
-                "id": k,
-                "name": v,
-                "supercategory": v
-            })
-        image_path = image_url.split('/')[-1]
-        image_id = 1 
-        coco_output["images"].append({
-            "id": image_id,
-            "license": 1,
-            "file_name": image_path,
-            "url" : image_url,
-            "height": height,
-            "width": width
-        })
+        for k, v in self.class_mapping.items():
 
-        annotation_id_counter = 1 
-        
+            coco_output["categories"].append(
+                {
+                    "id": k,
+                    "name": v,
+                    "supercategory": v,
+                }
+            )
+
+        image_id = 1
+        coco_output["images"].append(
+            {
+                "id": image_id,
+                "license": 1,
+                "file_name": image_path,
+                "height": height,
+                "width": width,
+            }
+        )
+
+        annotation_id_counter = 1
+
         for pred in predicted_boxes:
-            bbox_coords = pred['bbox']            
+            bbox_coords = pred["bbox"]
             x1, y1, x2, y2 = bbox_coords
-            
+
             coco_x = x1
             coco_y = y1
             coco_width = x2 - x1
             coco_height = y2 - y1
-            
+
             area = coco_width * coco_height
-            
-            coco_output["annotations"].append({
-                "id": annotation_id_counter,
-                "image_id": image_id,
-                "category_id": pred['category_id'],
-                "bbox": [coco_x, coco_y, coco_width, coco_height],
-                "area": area,
-                "segmentation": [], 
-                "iscrowd": 0 
-            })
-            
+
+            coco_output["annotations"].append(
+                {
+                    "id": annotation_id_counter,
+                    "image_id": image_id,
+                    "category_id": pred["category_id"],
+                    "bbox": [coco_x, coco_y, coco_width, coco_height],
+                    "area": area,
+                    "segmentation": [],
+                    "iscrowd": 0,
+                }
+            )
+
             annotation_id_counter += 1
 
         return coco_output
 
+
 # if __name__ == "__main__":
 #     class_mapping_path = "inference/class_mapping.json"
 #     modelpath = "inference/models/model.pt"
-#     image_url = "YOUR_IMAGE_LINK"
 #     predictor = FishDetector(
 #         class_mapping_path=class_mapping_path,
 #         modelpath=modelpath,
 #     )
-#     preds = predictor.predict(image_url=image_url)
+#     image_path = "YOUR_IMAGE_LINK OR YOUR_IMAGE_PATH"
+#     preds = predictor.predict(image_path=image_path)
 #     coco_annotation = predictor.get_coco_annotation(
-#         image_url=image_url,
-#         predicted_boxes=preds
+#         image_path=image_path, predicted_boxes=preds
 #     )
-#     with open("output.json", "w") as f:
+#     with open("output1.json", "w") as f:
 #         json.dump(coco_annotation, f, indent=4)
-    
